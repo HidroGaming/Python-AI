02d727f6e1369b221cd775e0e43adde80080af75
ItsPixel <81959865+HidroGaming@users.noreply.github.com>
2023-02-07 21:00:55 +0100

    Removed Useless code for github

--- model1.py
+++ model1.py
@@ -58,14 +58,6 @@ class Model(nn.Module):
 
 model = Model(input_shape=784,hidden_units=2,output_shape=len(train_data.classes)).to(device)
 
-if Path("helper_functions.py").is_file():
-    print("already downloaded")
-else:
-    print("Downloading helper_functions.py script")
-    request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
-    with open("helper_functions.py", "wb") as f:
-        f.write(request.content)
-
 
 LEARNING_RATE = 0.05
 
